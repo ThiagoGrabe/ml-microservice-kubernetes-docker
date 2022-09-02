8f89e338e8b9a10763b9792ab7c2d08d8822a193
Thiago Grabe <thiago.grabe@vetta.digital>
2022-09-02 09:26:50 -0300

    final commit

---
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=estopa/house-price-predictor:1.0
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl create deployment house-price-predictor-app --image=estopa/house-price-predictor:1.0
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+#Check if pod is running before proceeding further 
+ready=`kubectl get pods -o jsonpath="{.items[*].status.phase}"`
+while [ $ready != 'Running' ]
+do
+        echo "Waiting for Pod to be up"
+        sleep 5 
+        ready=`kubectl get pods -o jsonpath="{.items[*].status.phase}"`
+done
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward deployment/house-price-predictor-app --address 0.0.0.0 8000:80
